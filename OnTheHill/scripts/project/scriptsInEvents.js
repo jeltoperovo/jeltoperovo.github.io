150d7bbb853712b19790a6874eaf96e838d3397a
jeltoperovo <138780837+jeltoperovo@users.noreply.github.com>
2023-07-06 17:54:35 +0300

    Add files via upload

---
+++ OnTheHill/scripts/project/scriptsInEvents.js
@@ -0,0 +1,19 @@
+
+
+
+const scriptsInEvents = {
+
+	async EventMenu_Event3_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#423c59'
+	},
+
+	async EventGameplay_Event1_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#423c59'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
